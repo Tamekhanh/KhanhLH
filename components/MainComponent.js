1d38dc503416c642e33505eafe737da32be92a67
Tamek <khanh.lh06586@sinhvien.hoasen.edu.vn>
2025-10-01 09:15:10 +0700

    1.1.3 fixed

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import { DISHES } from './shared/dishes';
+import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
   constructor(props) {
